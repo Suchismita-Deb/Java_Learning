7fb7a0afac06aa5c65ca425622b7acbaca4bd6e9
Suchismita Deb <suchismitadeb2000@gmail.com>
2023-08-26 22:55:13 +0530

    array basic

--- src/array/A1.java
+++ src/array/A1.java
@@ -2,6 +2,19 @@ package array;
 
 public class A1 {
     public static void main(String[] args) {
+        int[] arr = new int[5];
+        for(int i=0;i<5;i++)
+        {
+            arr[i] = i+1;
+        }
+        for(int i:arr)
+            System.out.println(i);
 
+        System.out.println();
+
+        // Initializing the array.
+        int[] ar  = {1,2,3,4,5};
+        for(int i:ar)
+            System.out.println(i);
     }
 }

a48bb474eceb9745f5edb3d942710d5c4388ecb5
Suchismita Deb <suchismitadeb2000@gmail.com>
2023-08-30 09:22:36 +0530

    LinkedList

---
+++ src/string/S2.java
@@ -0,0 +1,17 @@
+package string;
+
+public class S2 {
+    public static void main(String[] args) {
+        //contains() method checks whether a string contains a sequence of characters.
+        String str1 = "DSA and Algorithm";
+        String str2 = "and";
+        // check if one string is present in another string.
+        System.out.println(str1.contains(str2)); // true if str1 contains str2.
+
+        // Check if two string is equal.
+        String str3 = "Java";
+        String str4 = "java";
+        boolean result = str3.equals(str4);
+        System.out.println(result);
+    }
+}

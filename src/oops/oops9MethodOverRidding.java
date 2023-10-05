16621eda99bcb306f192cc8dc458c017d67a94bf
Suchismita-Deb <suchismitadeb2000@gmail.com>
2023-10-06 01:35:06 +0530

    OOPs.

---
+++ src/oops/oops9MethodOverRidding.java
@@ -0,0 +1,29 @@
+package oops;
+
+class Vehicle{
+    public void run(){
+        System.out.println("Vehicle is running.");
+    }
+}
+class Car extends Vehicle{
+    public void run(){
+        System.out.println("Car is running.");
+    }
+
+}
+public class oops9MethodOverRidding {
+    public static void main(String[] args) {
+        Vehicle v = new Vehicle();
+        v.run();
+        // Vehicle is running.
+
+        Car c = new Car();
+        c.run();
+        // Car is running.
+
+        Vehicle vCar = new Car();
+        vCar.run();
+        // Car is running.
+        // Method will be called in the class written after the new keyword.
+    }
+}

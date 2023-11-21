ebca75277936a2e90db22b617a1ece91bc421d84
Suchismita-Deb <suchismitadeb2000@gmail.com>
2023-11-21 21:56:55 +0530

    removing idea folder.

---
+++ src/main/java/org/example/DailyDrill/A1/PrimitiveWrapper.java
@@ -0,0 +1,36 @@
+package org.example.DailyDrill.A1;
+
+public class PrimitiveWrapper {
+    public static void main(String[] args) {
+        // Primitive cannot store null.
+        int i=10;
+        // Wrapper can store null.
+        Integer j=null;
+        Integer n = 98;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(n);
+
+        // Autoboxing: int to Integer.
+        int primitiveInt = 89;
+        Integer wrapperInt = primitiveInt;
+
+        // Unboxing: Integer to int.
+        Integer anotherWrapperInt = 898;
+        int anotherPrimitiveInt = anotherWrapperInt;
+
+        short s = (short) 32768;//16 bits, -32768 to 32767
+        System.out.println(s);
+        int test = 90;
+        Integer wrapperTest =Integer.valueOf(test);// Explicit conversion.
+        Integer wrapperTest2 = test;// Autoboxing. Implicit conversion.
+        System.out.println(wrapperTest +" "+ wrapperTest2);
+
+        int size = 8;
+        int primitiveIntArray[] = new int[size]; // Primitive datatype.
+        Integer wrapperIntArray[] = new Integer[size]; // Wrapper datatype.
+        // int,char,float,boolean are primitive.
+        // Integer, Character, Float, Boolean are wrapper.
+
+    }
+}
---
+++ src/main/java/org/example/DailyDrill/streamLearning/S1StreamParallelStream.java
@@ -0,0 +1,19 @@
+package org.example.DailyDrill.streamLearning;
+
+import java.util.List;
+
+public class S1StreamParallelStream {
+    public static void main(String[] args) {
+        List<String> l = List.of("a","b","c","d","e");
+        //For sequential execution
+        l.stream().forEach(System.out::print);
+        System.out.println();
+
+        // Not considering the sequential execution
+        l.parallelStream().forEach(System.out::print);
+
+        //Output
+        //abcde
+        //cedba
+    }
+}

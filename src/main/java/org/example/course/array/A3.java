e694a4f80d8bdc6866bda05e0021b4d1cec06671
Suchismita-Deb <suchismitadeb2000@gmail.com>
2023-12-26 09:36:45 +0530

    Practice.

--- src/main/java/org/example/course/array/A3.java
+++ src/main/java/org/example/course/array/A3.java
@@ -6,6 +6,35 @@ public class A3 {
     public static void main(String[] args) {
         moveAllZeroEnd();
         check3ConsecutiveOdds();
+        productPrevNextEleUsingExtraSpace();
+
+        // Product of previous element and next element without using extra space.
+        int arr[] = {2,3,4,5,6};
+        int nums[] = new int[arr.length];
+        int j=1;
+//        arr[0] = arr[0]*arr[1];
+        nums[arr.length-1] = arr[arr.length-1]*arr[arr.length-2];
+        for(int i=1;i<arr.length-1;i++){
+
+            if(i==0) arr[0] = arr[0]*arr[1];
+            int temp = arr[i-1];
+            if(i!=0) arr[j++] = temp*arr[i+1];
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void productPrevNextEleUsingExtraSpace() {
+        // Array for every element it will be the product of the previous element and the next element.
+        int arr[] = {2,3,4,5,6};
+        int nums[] = new int[arr.length];
+        int j=1;
+        nums[0] = arr[0]*arr[1];
+        nums[arr.length-1] = arr[arr.length-1]*arr[arr.length-2];
+        for(int i=1;i<arr.length-1;i++){
+            nums[j++] = arr[i-1]*arr[i+1];
+        }
+        System.out.println(Arrays.toString(nums));
     }
 
     private static void check3ConsecutiveOdds() {
